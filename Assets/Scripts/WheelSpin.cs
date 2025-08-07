5ec09f3de74bbda183c5180d8e87dbd69c993299
Walsh, Ethan Joseph <ethan.walsh282003@gmail.com>
2025-08-07 16:59:56 -0400

    dice roll and color wheel
    plus some tmp importing i think

--- Assets/Scripts/WheelSpin.cs
+++ Assets/Scripts/WheelSpin.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class NewBehaviourScript : MonoBehaviour
+public class WheelSpin : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] int min = 1;
+    [SerializeField] int max = 10;
+    [SerializeField] TMP_Text number;
+    public int numberRolled;
 
-    // Update is called once per frame
-    void Update()
+    // Start is called before the first frame update
+    // void Start()
+    // {
+
+    // }
+
+    // // Update is called once per frame
+    // void Update()
+    // {
+
+    // }
+
+    public void OnButtonClick()
     {
-        
+        numberRolled = Random.Range(min, max);
+        number.text = numberRolled.ToString();
     }
 }
